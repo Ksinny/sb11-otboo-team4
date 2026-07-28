7a9eeeb24c06bd24ff548394835739cdddfcca8b
Ksinny <dosly2@naver.com>
2026-07-28 15:46:45 +0900

    test(green): 피드 목록 조회 커서 기본 조회 구현

--- src/main/java/com/sprint/mission/otboo/domain/social/feed/repository/querydsl/impl/FeedCustomRepositoryImpl.java
+++ src/main/java/com/sprint/mission/otboo/domain/social/feed/repository/querydsl/impl/FeedCustomRepositoryImpl.java
@@ -1,9 +1,14 @@
 package com.sprint.mission.otboo.domain.social.feed.repository.querydsl.impl;
 
+import static com.sprint.mission.otboo.domain.social.feed.entity.QFeed.feed;
+
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sprint.mission.otboo.domain.social.feed.dto.FeedListParams;
 import com.sprint.mission.otboo.domain.social.feed.entity.Feed;
 import com.sprint.mission.otboo.domain.social.feed.repository.querydsl.FeedCustomRepository;
+import com.sprint.mission.otboo.global.dto.SortDirection;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -16,6 +21,16 @@ public class FeedCustomRepositoryImpl implements FeedCustomRepository {
 
   @Override
   public List<Feed> findFeeds(FeedListParams params) {
-    return List.of();
+    Order order = params.sortDirection() == SortDirection.ASCENDING ? Order.ASC : Order.DESC;
+
+    return queryFactory
+        .selectFrom(feed)
+        .where(feed.softDeletable.deletedAt.isNull())
+        .orderBy(
+            new OrderSpecifier<>(order, feed.createdAt),
+            new OrderSpecifier<>(order, feed.id)
+        )
+        .limit(params.limit() + 1L)
+        .fetch();
   }
 }
