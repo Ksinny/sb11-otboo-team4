a72ac0a608b511c6d29e12f3c6bd82efd705eafe
Ksinny <dosly2@naver.com>
2026-07-30 19:55:09 +0900

    test(green): 팔로우 생성 컨트롤러 201 응답 구현

--- src/main/java/com/sprint/mission/otboo/domain/social/follow/controller/FollowController.java
+++ src/main/java/com/sprint/mission/otboo/domain/social/follow/controller/FollowController.java
@@ -6,17 +6,19 @@ import com.sprint.mission.otboo.domain.social.follow.dto.FollowDto;
 import com.sprint.mission.otboo.domain.social.follow.service.FollowService;
 import com.sprint.mission.otboo.global.security.jwt.filter.CurrentUser;
 import com.sprint.mission.otboo.global.security.jwt.filter.UserPrincipal;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/follows")
+@Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/api/follows")
+@RestController
 public class FollowController implements FollowApi {
 
   private final FollowService followService;
@@ -24,8 +26,10 @@ public class FollowController implements FollowApi {
   @PostMapping
   @Override
   public ResponseEntity<FollowDto> createFollow(
-      @Valid @RequestBody FollowCreateRequest request,
+      @RequestBody FollowCreateRequest request,
       @CurrentUser UserPrincipal principal) {
-    return null; // TODO(green)
+    log.debug("팔로우 생성 요청: userId={}", principal.userId());
+    FollowDto created = followService.create(request, principal.userId());
+    return ResponseEntity.status(HttpStatus.CREATED).body(created);
   }
 }
