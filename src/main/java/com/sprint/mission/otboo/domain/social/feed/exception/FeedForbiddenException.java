2dcdf7a5f45da3c249312ebdaaf9484299f3877b
Ksinny <dosly2@naver.com>
2026-08-04 11:20:44 +0900

    fix: FeedForbiddenException 메시지 오타 수정

--- src/main/java/com/sprint/mission/otboo/domain/social/feed/exception/FeedForbiddenException.java
+++ src/main/java/com/sprint/mission/otboo/domain/social/feed/exception/FeedForbiddenException.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 
 public class FeedForbiddenException extends FeedException {
 
-  private static final String MESSAGE = "본인만 수행할 수 있습니다..";
+  private static final String MESSAGE = "본인만 수행할 수 있습니다.";
 
   private FeedForbiddenException(Map<String, Object> details) {
     super(HttpStatus.FORBIDDEN, MESSAGE, details);
