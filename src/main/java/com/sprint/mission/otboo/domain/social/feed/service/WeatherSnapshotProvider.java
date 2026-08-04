ddd608cb44836275219f2979318d9076e67c488e
Ksinny <dosly2@naver.com>
2026-08-04 11:34:09 +0900

    refactor: SnapshotProvider에 읽기전용 트랜잭션 추가

--- src/main/java/com/sprint/mission/otboo/domain/social/feed/service/WeatherSnapshotProvider.java
+++ src/main/java/com/sprint/mission/otboo/domain/social/feed/service/WeatherSnapshotProvider.java
@@ -8,10 +8,12 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
+@Transactional(readOnly = true)
 public class WeatherSnapshotProvider {
 
   private final WeatherRepository weatherRepository;
