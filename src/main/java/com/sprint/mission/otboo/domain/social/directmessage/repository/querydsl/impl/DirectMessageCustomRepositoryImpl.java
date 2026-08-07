430e6e7dff89dfc1fbd250541000953868490a4f
Ksinny <dosly2@naver.com>
2026-08-07 18:51:55 +0900

    test(green): DM 양방향 커서 목록 조회 구현

--- src/main/java/com/sprint/mission/otboo/domain/social/directmessage/repository/querydsl/impl/DirectMessageCustomRepositoryImpl.java
+++ src/main/java/com/sprint/mission/otboo/domain/social/directmessage/repository/querydsl/impl/DirectMessageCustomRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.sprint.mission.otboo.domain.social.directmessage.repository.querydsl.impl;
 
+import static com.sprint.mission.otboo.domain.social.directmessage.entity.QDirectMessage.directMessage;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sprint.mission.otboo.domain.social.directmessage.dto.DirectMessageParams;
 import com.sprint.mission.otboo.domain.social.directmessage.entity.DirectMessage;
@@ -7,6 +10,7 @@ import com.sprint.mission.otboo.domain.social.directmessage.repository.querydsl.
 import com.sprint.mission.otboo.global.dto.CursorPageResponse;
 import com.sprint.mission.otboo.global.dto.SortDirection;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 
@@ -18,7 +22,34 @@ public class DirectMessageCustomRepositoryImpl implements DirectMessageCustomRep
   @Override
   public CursorPageResponse<DirectMessage> findDirectMessages(UUID currentUserId,
       DirectMessageParams params) {
-    return new CursorPageResponse<>(List.of(), null, null, false, 0L,
-        "createdAt", SortDirection.DESCENDING);
+    List<DirectMessage> page = fetchDirectMessages(currentUserId, params);
+
+    return new CursorPageResponse<>(page, null, null, false,
+        countDirectMessages(currentUserId, params.userId()), "createdAt", SortDirection.DESCENDING
+    );
+  }
+
+  private List<DirectMessage> fetchDirectMessages(UUID currentUserId,
+      DirectMessageParams params) {
+    return queryFactory
+        .selectFrom(directMessage)
+        .where(betweenUsers(currentUserId, params.userId()))
+        .orderBy(directMessage.createdAt.desc(), directMessage.id.desc())
+        .fetch();
+  }
+
+  private long countDirectMessages(UUID currentUserId, UUID otherUserId) {
+    return Optional.ofNullable(
+        queryFactory.select(directMessage.count())
+            .from(directMessage)
+            .where(betweenUsers(currentUserId, otherUserId))
+            .fetchOne()
+    ).orElse(0L);
+  }
+
+  // 발신/수신 방향 모두 포함
+  private BooleanExpression betweenUsers(UUID one, UUID other) {
+    return directMessage.senderId.eq(one).and(directMessage.receiverId.eq(other))
+        .or(directMessage.senderId.eq(other).and(directMessage.receiverId.eq(one)));
   }
 }
