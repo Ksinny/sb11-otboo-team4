d33b16e7401e407f5277a8db44c4cb9937e124bc
Ksinny <dosly2@naver.com>
2026-08-09 11:47:41 +0900

    test(red): STOMP destination 사전순 정렬 케이스 실패 테스트

---
+++ src/main/java/com/sprint/mission/otboo/domain/social/directmessage/util/StompDestinationUtil.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.otboo.domain.social.directmessage.util;
+
+import java.util.UUID;
+
+public final class StompDestinationUtil {
+
+  private StompDestinationUtil() {
+  }
+
+  public static String directMessageDestination(UUID one, UUID other) {
+    return null;
+  }
+}
